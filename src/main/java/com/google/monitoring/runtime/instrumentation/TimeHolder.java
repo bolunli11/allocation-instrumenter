a81d84688369587a3539ad4a36f1b873b2975cee
bolunli11 <bli35@ncsu.edu>
2021-07-11 13:23:16 -0400

    add native method in TimeHolder

--- src/main/java/com/google/monitoring/runtime/instrumentation/TimeHolder.java
+++ src/main/java/com/google/monitoring/runtime/instrumentation/TimeHolder.java
@@ -4,12 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 public class TimeHolder {
     private static Map<String, Long> timeCache = new HashMap<>();
+
+    public native void clearTree2();
+
     public static void start(String method) {
+        TimeHolder ai = new  TimeHolder();
+        ai.clearTree2();
+
         System.out.println("Method start: " + method);
         timeCache.put(method, System.currentTimeMillis());
     }
+
     public static long cost(String method) {
         System.out.println("Method end: " + method);
         return System.currentTimeMillis() - timeCache.get(method);
     }
+
 }
